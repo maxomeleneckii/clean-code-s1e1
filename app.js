519c4b3221b49fb3f67f49c3054658d147b6f3c2
Max Omeleneckii <max.omeleneckii@gmail.com>
2022-05-16 16:00:41 +0300

    refactor:change label and input tags in CSS according to clause 3.4 html-and-css.md

--- app.js
+++ app.js
@@ -37,8 +37,9 @@ var createNewTaskElement = function (taskString) {
 
     //Each elements, needs appending
     checkBox.type = "checkbox";
+    checkBox.className = "checkbox";
     editInput.type = "text";
-    editInput.className = "task";
+    editInput.className = "task text";
 
     editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
     editButton.className = "edit btn";
