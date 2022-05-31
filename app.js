28b933d5cd668291ae6f1ad051003717efceb852
Max Omeleneckii <max.omeleneckii@gmail.com>
2022-05-31 16:36:27 +0300

    refactor: add alt attribute to images generated with js according to clause 1.2 html-and-css-extended.md

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement = function (taskString) {
     deleteButton.className = "delete btn";
     deleteButtonImg.src = './remove.svg';
     deleteButtonImg.className = "img";
+    deleteButtonImg.alt = "icon remove";
     deleteButton.appendChild(deleteButtonImg);
 
 
